445039b16dff3a1442677ff3a79e77bd537b8f80
Jimmy <jimmyxu1997@berkeley.edu>
2021-03-21 16:59:21 +0800

    [feat]Add result page; Enable recording audio

--- app/app.py
+++ app/app.py
@@ -11,13 +11,16 @@ def index():
     """
     return render_template("app/home.html")
 
+
 @app.route("/speak")
 def speak():
     """
     The page for the user to input their voice.
     """
-    script = request.args.get('script')
-    return render_template("app/speak.html", script=script)
+    data = {}
+    data['script'] = request.args.get('script')
+    return render_template("app/speak.html", data=data)
+
 
 @app.route("/uploadLine", methods=["POST"])
 def uploadLine():
@@ -27,8 +30,29 @@ def uploadLine():
     """
     if request.method == "POST":
         script = request.form['script']
-        print(script)
         return redirect(url_for('speak', script=script))
+
+
+@app.route("/uploadAudio", methods=['POST'])
+def uploadAudio():
+    if request.method == "POST":
+        f = request.files['audio_data']
+        with open('audio.wav', 'wb') as audio:
+            suggested_sentiment, user_sentiment = runModel(audio)
+            return redirect(url_for('showResult', suggested_sentiment=suggested_sentiment, user_sentiment=user_sentiment))
+
+
+@app.route("/showResult")
+def showResult():
+    data = {}
+    data['suggested_sentiment'] = request.args.get('suggested_sentiment')
+    data['user_sentiment'] = request.args.get('user_sentiment')
+    return render_template("app/result.html", data=data)
+
+# @app.route("/runModel", methods=["POST"])
+def runModel():
+    # Get audio file
+    return 1, 1
 
 if __name__ == "__main__":
     app.run()
